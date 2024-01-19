9372bacad111f4d82ebbab400398722d72543e7d
Ben Furner <bfurner27@yahoo.com>
2024-01-18 17:10:40 -0700

    Adding CORS policy for dev backend

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
 from typing import Annotated
 
-from fastapi import Depends, FastAPI
-from fastapi.security import OAuth2PasswordBearer
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.features.country.router import router as countryRouter
 from src.features.earthquake.router import router as earthQuakeRouter
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:2402",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(countryRouter, prefix='/countries', tags=['country'])
 app.include_router(earthQuakeRouter, prefix='/earthquakes', tags=['earthquake'])
